08d858b41f2fa6582fdfbeb6972ae4d9d19ce320
Sola0822 <dldpthf2003@naver.com>
2024-08-11 11:56:05 +0900

    수정관&성신관 층별 안내도 반영

--- asset/Modal.js
+++ asset/Modal.js
@@ -1,10 +1,25 @@
-    //팝업 열기
-    function openModal(modalname) {
-      document.get
-      $("." + modalname).fadeIn(300);
-  }
+document.addEventListener('DOMContentLoaded', function () {
+  const buttons = document.querySelectorAll('.btn');
+  const modals = document.querySelectorAll('.modal-box');
+  const closeButtons = document.querySelectorAll('.close');
 
-// 팝업 닫기
-$('.popup .close').click(function() {
-      $(this).parent().fadeOut(300);
+  buttons.forEach(button => {
+      button.addEventListener('click', function () {
+          const classList = this.classList;
+          modals.forEach(modal => {
+              classList.forEach(className => {
+                  if (modal.classList.contains(className.replace('-btn', '-modal'))) {
+                      modal.classList.add('active');
+                  }
+              });
+          });
+      });
   });
+
+  closeButtons.forEach(button => {
+      button.addEventListener('click', function () {
+          const modal = this.closest('.modal-box'); // 가장 가까이 있는 모달박스 선택
+          modal.classList.remove('active'); //active 제거
+      });
+  });
+});
